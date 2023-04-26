6813571038a4fc12663e38d1904a4ce1b831fddb
JaviCastillo <jav.castonatt@gmail.com>
2023-04-26 16:08:05 -0400

    actualizar contenido

--- README.md
+++
@@ -1,9 +0,0 @@
-# A Nimrodel Pub Restaurant
-
-Sitio web creado como Proyecto para el Bootcamp de Talento Digital.
-
-Sitio simula la vista principal del sitio de un Restaurante ficticio, 
-el cual debe mostrar los productos, y contar con formularios para Contacto y Reserva.
-El sitio utiliza elementos de Bootstrap en conjunción con elementos creados prioritariamente para este proyecto.
-
-Sitio se encuentra disponible para vistar en https://javicastillo.github.io/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,161 +1,61 @@
-let mostrarContact = () => {
-    let form = document.getElementById('contact');
-    form.classList.remove('d-none');
-}
+$(document).ready(function () {
 
-let mostrarReserva = () => {
-    let form = document.getElementById('reserva');
-    form.classList.remove('d-none');
-}
+    $('html').addClass('active');
+    $('body').addClass('active');
 
-let submitContact = (event) => {
-
-    event.preventDefault();
-
-    let form = document.getElementById('contact');
-
-    let name = document.getElementById('nombre').value;
-    let email = document.getElementById('correo').value;
-    let phone = document.getElementById('telefono').value;
-    let motiv = document.getElementById('motivo').value;
-    let area = document.getElementById('motivo2').value;
-
-    let nono = "";
-
-    switch (nono) {
-
-        case name:
-            alert(`El campo 'Nombre' es obligatorio`);
-            return false;
-            break;
-
-        case email:
-            alert(`El campo 'Correo' es obligatorio`);
-            return false;
-            break;
-
-        case phone:
-            alert(`El campo 'Telefono' es obligatorio`);
-            return false;
-            break;
-
-        case motiv:
-            alert(`El campo 'Motivo' es obligatorio`);
-            return false;
-            break;
-
-        case area:
-            alert(`El campo 'Detalle del Motivo' es obligatorio`);
-            return false;
-            break;
-
-        default:
-            alert(`Muchas gracias ${name}, hemos recibido tu comentario, y enviaremos una pronta respuesta al correo ${email}`);
-            form.reset();
-            form.classList.add('d-none');
-            break;
+    let centerCarouselItems = () => {
+        var carouselHeight = $('#main-carousel').height();
+        $('.carousel-inner').each((e) => {
+            var itemHeight = $(this).find('.carousel-item.active').height();
+            var topMargin = (carouselHeight - itemHeight) / 2;
+            $(this).find('.carousel-item').css('margin-top', topMargin);
+        });
     }
 
-}
+    centerCarouselItems();
 
-$(document).ready(() => {
 
-    $('#reserva').submit((e) => {
-        e.preventDefault();
+    
+        $('#main-carousel').on('slide.bs.carousel', function (e) {
 
-        let form = $('#reserva');
-        let name = $('#nombreres').val();
-        let email = $('#correores').val();
-        let phone = $('#telefonores').val();
-        let fecha = $('#fecha').val();
-        let hora = $('#hora').val();
-        let ctda = $('#ctda').val();
+            var newIndex = $(e.relatedTarget).index();
+            $('.nav-link').removeClass('active');
+            $('.nav-link[data-slide-to="' + newIndex + '"]').addClass('active');
+            
+        });
+    
 
-        let nono = "";
 
-        switch (nono) {
-
-            case name:
-                alert(`El campo 'Nombre' es obligatorio`);
-                return false;
-                break;
-
-            case email:
-                alert(`El campo 'Correo' es obligatorio`);
-                return false;
-                break;
-
-            case phone:
-                alert(`El campo 'Telefono' es obligatorio`);
-                return false;
-                break;
-
-            case fecha:
-                alert(`El campo 'Fecha' es obligatorio`);
-                return false;
-                break;
-
-            case hora:
-                alert(`El campo 'Hora' es obligatorio`);
-                return false;
-                break;
-
-            case ctda:
-                alert(`El campo 'Cantidad' es obligatorio`);
-                return false;
-                break;
-
-            default:
-                alert(`Estimad@ ${name}, agradecemos tu reserva con nosotros.
-
-Hemos registrado ${ctda} asistentes para el ${fecha} a la hora ${hora}.
-Se ha enviado el codigo de confimación al correo ${email}.
-
-Gracias por preferirnos.`);
-                form[0].reset();
-                form.addClass('d-none');
-                break;
-        }
-
+    $(function () {
+        $('html').keydown(function (e) {
+            switch (e.key) {
+                case 'ArrowLeft':
+                    $('.carousel').carousel('prev')
+                    break;
+                case 'ArrowRight':
+                    $('.carousel').carousel('next')
+                    break;
+            }
+        });
     });
 
 
-    $('.card').mousedown( (e) => { 
-        $(e.currentTarget).addClass('shadow-sm');
-        $(e.currentTarget).removeClass('shadow');
-
-        $('#sobreNosotros').removeClass('active');
-        $('#cartaProductos').addClass('active');
-
-        let imagen = $(e.currentTarget).children('img').attr('src');
-        let titulo = $(e.currentTarget).children('div').children('h5').text();
-        let texto = $(e.currentTarget).children('div').children('p').text();
-
-        console.log(titulo);
-
-        $('#cartaProductos').children('img').attr('src', imagen);
-        $('#cartaProductos').children('h3').text(titulo);
-        $('#cartaProductos').children('p').text(texto);
-        
-
+    $('#main-carousel').on('touchstart', function(event){
+        const xClick = event.originalEvent.touches[0].pageX;
+        $(this).one('touchmove', function(event){
+            const xMove = event.originalEvent.touches[0].pageX;
+            const sensitivityInPx = 5;
+    
+            if( Math.floor(xClick - xMove) > sensitivityInPx ){
+                $(this).carousel('next');
+            }
+            else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+                $(this).carousel('prev');
+            }
+        });
+        $(this).on('touchend', function(){
+            $(this).off('touchmove');
+        });
     });
-
-    $('.card').mouseup( (e) => { 
-        $(e.currentTarget).addClass('shadow');
-        $(e.currentTarget).removeClass('shadow-sm');
-    });
-
-    $('#nosotros').mousedown( () => { 
-        $('#cartaProductos').removeClass('active');
-        $('#sobreNosotros').addClass('active');
-    });
-
-    $('.fa-close').mousedown( () => { 
-        $('#cartaProductos').removeClass('active');
-        $('#sobreNosotros').removeClass('active');
-    });
-
-
-
 
 });
